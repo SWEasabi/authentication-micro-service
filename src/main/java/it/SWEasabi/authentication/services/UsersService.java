8672e393523067ddb3c31a46afee3c40257c8e6a
Michele1207 <44138357+bonni1207@users.noreply.github.com>
2023-05-31 17:17:50 +0200

    Fork non ha caricato le modifiche

--- src/main/java/it/SWEasabi/authentication/services/UsersService.java
+++
@@ -1,7 +0,0 @@
-package it.SWEasabi.authentication.services;
-
-public interface UsersService
-{
-    // ritorna l'hash della password dell'utente, se esiste, altrimenti stringa vuota
-    public String getPasswordHashFromUser(String username);
-}
