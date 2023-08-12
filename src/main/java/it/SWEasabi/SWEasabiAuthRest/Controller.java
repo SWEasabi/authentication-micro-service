c221f602e36ae3a330e1d5696a8faaa986d346e0
casYy <51257666+casYy@users.noreply.github.com>
2023-08-12 15:47:46 +0200

    dependency injection 3

--- src/main/java/it/SWEasabi/SWEasabiAuthRest/Controller.java
+++ src/main/java/it/SWEasabi/SWEasabiAuthRest/Controller.java
@@ -1,10 +1,12 @@
 package it.SWEasabi.SWEasabiAuthRest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import it.SWEasabi.authentication.CoreAuthService;
+import it.SWEasabi.authentication.entities.AuthUser;
 import it.SWEasabi.authentication.kernel.LoginResult;
 import it.SWEasabi.authentication.services.BlacklistService;
 import it.SWEasabi.authentication.services.KeysService;
@@ -19,23 +21,28 @@ import com.google.gson.JsonObject;
 @RestController
 public class Controller
 {
-	private final CoreAuthService core;
-	UsersService usersService = new LocalUsersService();
-	KeysService keysService = new LocalKeysService();
-	BlacklistService blacklistService = new LocalBlacklistService();
+	private CoreAuthService core;
+	
+	//UsersService usersService = new LocalUsersService();
+	//KeysService keysService = new LocalKeysService();
+	//BlacklistService blacklistService = new LocalBlacklistService();
 
-	public Controller()
+	@Autowired
+	public Controller(UsersService usersService, KeysService keysService, BlacklistService blacklistService)
 	{
 		core = new CoreAuthService(usersService, keysService, blacklistService);
 	}
 
 	@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 	@PostMapping("/login")
-	String LoginListener(@RequestBody String json)
+	String LoginListener(@RequestBody AuthUser user)
 	{
-		JsonObject rq = new Gson().fromJson(json, JsonObject.class);
-		String username = rq.get("username").toString();
-		String password = rq.get("password").toString();
+		//JsonObject rq = new Gson().fromJson(json, JsonObject.class);
+		//String username = rq.get("username").toString();
+		//String password = rq.get("password").toString();
+		
+		String username = user.getUsername();
+		String password = user.getPassword();
 		
 		LoginResult result = core.login(username, password);
 
