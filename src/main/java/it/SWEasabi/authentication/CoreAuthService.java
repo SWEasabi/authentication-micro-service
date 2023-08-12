e2154b77a6004ee0148f1c5a635d9db2bc4d1fa3
casYy <51257666+casYy@users.noreply.github.com>
2023-08-12 15:47:06 +0200

    dependency injection 2

--- src/main/java/it/SWEasabi/authentication/CoreAuthService.java
+++ src/main/java/it/SWEasabi/authentication/CoreAuthService.java
@@ -1,5 +1,7 @@
 package it.SWEasabi.authentication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import it.SWEasabi.authentication.kernel.Authenticator;
 import it.SWEasabi.authentication.kernel.JwtAuthority;
 import it.SWEasabi.authentication.kernel.LoginResult;
